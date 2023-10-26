2f4852f73da90a19f4c66ff415581a96cc3766e9
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-10-26 16:27:28 -0600

    final

--- Labboratorio10/app/src/main/java/com/example/labboratorio10/AuthActivity.kt
+++ Labboratorio10/app/src/main/java/com/example/labboratorio10/AuthActivity.kt
@@ -1,25 +1,47 @@
 package com.example.labboratorio10
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class AuthActivity : AppCompatActivity() {
-    val button = findViewById<Button>(R.id.button)
-    val button2 = findViewById<Button>(R.id.button2)
-    val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
-    val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+    private val GOOGLE_SIGN_IN =100
+    private lateinit var button:Button
+    private lateinit var button2 :Button
+    private lateinit var button3 :Button
+    private lateinit var editTextEmail :EditText
+    private lateinit var editTextPassword :EditText
+    private lateinit var authLayout:LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-     
+
 
         setContentView(R.layout.activity_auth)
+
+        button = findViewById(R.id.button)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        authLayout= findViewById(R.id.linearLayout)
         setup()
     }
+    override fun onStart(){
+        super.onStart()
+        authLayout.visibility=View.VISIBLE
+    }
+
 
     private fun setup(){
         title="Autenticación"
@@ -47,6 +69,13 @@ class AuthActivity : AppCompatActivity() {
                 }
             }
         }
+        button3.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
+
 
     }
     private fun showAlert(){
@@ -65,4 +94,32 @@ class AuthActivity : AppCompatActivity() {
         }
         startActivity(homeIntent)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==GOOGLE_SIGN_IN){
+            val task=GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                if(account!= null){
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showHome(account.email ?: "", ProviderType.GOOGLE)
+                        }
+                        else{
+                            showAlert()
+                        }
+                    }
+                }
+
+            }catch(e: ApiException){
+                showAlert()
+
+            }
+
+
+        }
+    }
+
 }
--- Labboratorio10/app/src/main/java/com/example/labboratorio10/HomeActivity.kt
+++ Labboratorio10/app/src/main/java/com/example/labboratorio10/HomeActivity.kt
@@ -1,5 +1,7 @@
 package com.example.labboratorio10
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,27 +9,45 @@ import android.widget.EditText
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 
-enum class ProviderType{
-    BASIC
+enum class ProviderType {
+    BASIC,
+    GOOGLE
 }
 
 class HomeActivity : AppCompatActivity() {
-    val button = findViewById<Button>(R.id.button)
-    val emailView = findViewById<TextView>(R.id.textView2)
-    val providerView = findViewById<TextView>(R.id.textView)
+    private lateinit var button :Button
+    private lateinit var emailView :TextView
+    private lateinit var providerView:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val bundle: Bundle?= intent.extras
-        val email=bundle?.getString("email")
-        val provider=bundle?.getString("provider")
-        setup(email?:"",provider?:"")
+
+         button = findViewById(R.id.button)
+         emailView = findViewById(R.id.textView2)
+        providerView = findViewById(R.id.textView)
+
+        val bundle: Bundle? = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("email", email)
+        editor.putString("provider", provider)
+        editor.apply()
     }
-    private fun setup(email:String,provider:String){
-        title="Inicio"
-        emailView.text=email
-        providerView.text=provider
-        button.setOnClickListener{
+
+    private fun setup(email: String, provider: String) {
+        title = "Inicio"
+        emailView.text = email
+        providerView.text = provider
+        button.setOnClickListener {
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+            val editor = prefs.edit()
+            editor.clear()
+            editor.apply()
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
